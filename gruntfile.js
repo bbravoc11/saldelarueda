a6a4009992a4f19bfccda58a1a84748413ae4059
Bernardo <bernardobravo11@gmail.com>
2019-03-14 20:00:53 +0100

    basic package + grunt complete

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,8 @@
 module.exports = function(grunt) {
-  require('jit-grunt')(grunt);
+
+  require('jit-grunt')(grunt,{
+  	useminPrepare: 'grunt-usemin'
+  });
 
   grunt.initConfig({
     less: {
@@ -10,13 +13,13 @@ module.exports = function(grunt) {
           optimization: 2
         },
         files: {
-          "css/styles.css": "css/styles.less" // destination file and source file
+          "css/styles.css":"css/styles.less" // destination file and source file
         }
       }
     },
    watch: {
       styles: {
-        files: 'css/styles.less',
+        files: "css/styles.less",
         tasks: ['less']
       }
     },
@@ -33,10 +36,110 @@ module.exports = function(grunt) {
 
     		}
     	}
+    },
+    copy: {
+    	html: {
+    		files: [{
+    			expand: true,
+    			dot: true,
+    			cwd: './',
+    			src: ['*.html'],
+    			dest: 'dist'
+    		}]
+    	},
+    	fonts: {
+    		files: [{
+    			expand: true,
+    			dot: true,
+    			cwd: 'node_modules/font-awesome',
+    			src: ['fonts/*.*'],
+    			dest: 'dist'
+    		}]
+    	}
+
+    }, 
+    clean: {
+    		build: {
+    		src: ['dist/']
+    		}
+    },
+    imagemin: {
+    	dynamic: {
+    		expand: true,
+    		dot: true,
+    		cwd: './',
+    		src: ['img/*.{png,jpg,gif}'],
+    		dest: 'dist/'
+
+    	}
+    },
+    useminPrepare: {
+    	foo: {
+    		dest: 'dist',
+    		src: ['index.html','second.html']
+    	},
+    	options: {
+    		flow: {
+    			steps: {
+    				css:['cssmin'],
+    				js:['uglify'],
+    			},
+    			post: {
+    				css: [{
+    					name: 'cssmin',
+    					createConfig: function (context, block){
+    						var generated = context.options.generated;
+    						generated.options = {
+    							keepSpecialComments: 0, rebase: false
+    						};
+    					}
+    				}]
+    			}
+    		}
+    	}
+    },
+    concat: {
+    	options: {
+    		separator: ';'
+    	},
+    	dist: {}
+    },
+    uglify: {
+    	dist: {}
+    },
+    cssmin: {
+    	dist:{}
+    },
+    filerev: {
+    	options: {
+    		encoding: 'utf8',
+    		algorithm: 'md5',
+    		length: 20
+    	},
+    	release: {
+    		src: ['dist/js/*.js','dist/css/*.css']
+    	}
+    },
+    usemin: {
+    	html:['dist/index.html','dist/second.html'],
+    	options: {
+    		assetsDirs: ['dist','dist/css','dist/js']
+    	}
+    },
+    htmlmin: {
+    	dist: {
+    		options:{
+    			collapseWhitespace: true
+    		},
+    		files:{
+    			'dist/index.html':'dist/index.html'
+    		}
+    	}
     }
     
   });
 
   grunt.registerTask('compile', ['less']);
-  grunt.registerTask('default', ['browserSync','watch']);
+  grunt.registerTask('default', ['watch','browserSync']);
+  grunt.registerTask('build', ['clean','copy','imagemin','useminPrepare','concat','cssmin','uglify','filerev','usemin','htmlmin']);
 };
