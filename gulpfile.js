76fa139f38a059723cc6a6a89e7e7eaa07652368
Bernardo <bernardobravo11@gmail.com>
2019-03-27 21:44:33 +0100

    Course 1 Final

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
-var gulp = require('gulp');
-var less = require('gulp-less'); 
-var browserSync = require('browser-sync');
+var gulp = require('gulp'),
+	less = require('gulp-less'), 
+	browserSync = require('browser-sync'),
+	del = require('del'),
+	imagemin = require('gulp-imagemin'),
+	uglify = require('gulp-uglify'),
+	usemin = require('gulp-usemin'),
+	rev = require('gulp-rev'),
+	cleanCss = require('gulp-clean-css'),
+	flatmap = require('gulp-flatmap'),
+	htmlmin = require('gulp-htmlmin')
+	;
  
  
 /* Task to compile less */
@@ -28,9 +37,51 @@ gulp.task('browser-sync',function(){
 		}
 	});
 });
+
 gulp.task('default',gulp.series('watch-less'), function() {
 	gulp.start('browser-sync');
 });
+
+gulp.task('clean', function(){
+	return del(['dist']);
+});
+
+gulp.task('copyfonts', async function(){
+	return gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}')
+	.pipe(gulp.dest('./dist/fonts'));
+});
+
+gulp.task('imagemin', async function(){
+	gulp.src('./img/*.{png,jpg,gif}')
+	.pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+	.pipe(gulp.dest('./dist/img'));
+});
+
+gulp.task('usemin', async function() {
+  return gulp.src('./*.html')
+  .pipe(flatmap(function(stream, file){
+      return stream
+        .pipe(usemin({
+            css: [ cleanCss(), rev() ],
+            html: [ function() { return htmlmin({ collapseWhitespace: true })} ],
+            js: [ uglify(), rev() ],
+            inlinejs: [ uglify() ],
+            inlinecss: [ cleanCss(), 'concat' ]
+        }))
+    }))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('build',gulp.series('clean','copyfonts','imagemin','usemin'));
+
+
+
+
+
+
+
+
+
 
 
 
